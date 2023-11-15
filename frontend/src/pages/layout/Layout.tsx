c15ddbc478db22ad7e92b23475716c17c6a9b76b
Wolfgang Prinz <wo-prinz@t-online.de>
2023-11-15 14:22:55 +0100

    title updated

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -80,7 +80,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>Bob der Web3 Bot</h1>
+                            <h1 className={styles.headerTitle}>Bob, der Web3 Bot</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
